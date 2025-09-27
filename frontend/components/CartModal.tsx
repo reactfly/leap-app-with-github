4dc36024ff1a3d7cd2318d300303fbeb6ae626f4
reactfly <studioreactfly@gmail.com>
2025-09-27 05:00:19 -0300

    botão finalizar pedido redireciona para delivery

--- frontend/components/CartModal.tsx
+++ frontend/components/CartModal.tsx
@@ -223,7 +223,7 @@ export default function CartModal({ isOpen, onClose }: CartModalProps) {
                 onClick={() => setIsDeliveryOpen(true)}
               >
                 <Truck className="w-5 h-5 mr-2" />
-                Finalizar Pedido - Delivery
+                Finalizar Pedido
               </Button>
               <Button 
                 variant="outline"
